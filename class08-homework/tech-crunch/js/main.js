ca3ab714c9221563605275bee3670c1e059a09af
Anna Burnett <annairishburnett@gmail.com>
2024-01-02 17:08:32 -0500

    updated width on images in news section

---
+++ class08-homework/tech-crunch/js/main.js
@@ -0,0 +1,11 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+
+    sidebar.style.display = 'block'
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+
+    sidebar.style.display = 'none'
+}
