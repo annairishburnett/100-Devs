abaeea600ec5500d9ccf632eb4bb50da6bf0858a
Anna Burnett <annairishburnett@gmail.com>
2025-01-21 17:31:38 -0500

    updated file structure and added new homework folder for class26

---
+++ class26/objects-shots/js/main.js
@@ -0,0 +1,28 @@
+//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+// const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
+
+document.querySelector('#getCocktail').addEventListener('click', getDrink)
+
+function getDrink() {
+    const drink = document.querySelector('input').value;
+
+    //push homework, make it cycle through the drinks
+    //try to get it to work with drinks that have spaces in their names (not using template literal)
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data.drinks[0]);
+        // console.log(data.drinks[0].strDrinkThumb)
+        document.querySelector('img').src = data.drinks[0].strDrinkThumb;
+        document.querySelector('h2').innerText = data.drinks[0].strDrink;
+        document.querySelector('p').innerText = data.drinks[0].strInstructions;
+
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
+
+
+
+
