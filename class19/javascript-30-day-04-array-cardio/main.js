b0d35de19a656fc2a76ff34c9612fb1545f58e39
Anna Burnett <annairishburnett@gmail.com>
2025-01-22 13:25:08 -0500

    finished array cardio homework assignment

--- class19/javascript-30-day-04-array-cardio/main.js
+++ class19/javascript-30-day-04-array-cardio/main.js
@@ -6,25 +6,19 @@
 
 const inventors = [
   { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
-  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },//died at 84
   { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
   { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
   { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
   { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
-  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },//died at 89
   { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
-  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
-  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
-  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },//died at 37
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },//died at 50
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },//died at 90
   { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
 ];
 
-
-function firstLastNames(arr){
-  
-}
-
-console.log(firstLastNames(inventors));
 
 const people = [
   'Bernhard, Sandra', 'Bethea, Erin', 'Becker, Carl', 'Bentsen, Lloyd', 'Beckett, Samuel', 'Blake, William', 'Berger, Ric', 'Beddoes, Mick', 'Beethoven, Ludwig',
@@ -51,27 +45,130 @@ console.log(bornIn1500(inventors));
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
 
-// function firstLastNames(arr){
+function firstLastNames(arr){
+  return arr.map((name) => `${name.first} ${name.last}`);
+}
 
-// }
+console.log(firstLastNames(inventors));
 
-// console.log(firstLastNames(inventors));
+
+
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
 
+function oldToYoung(arr){
+  return arr.sort((a,b) => a.year - b.year)
+}
+
+//Their solution was:
+// const ordered = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
+//Mine works too, though
+
+console.log(oldToYoung(inventors))
+
+
+
+
+
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
 
+function totalYearsLived(arr){
+  return arr.reduce((acc, c) => acc + (c.passed - c.year), 0);
+}
+
+console.log(totalYearsLived(inventors))
+
+
+
 // 5. Sort the inventors by years lived
+function yearsLived(arr){
+  return arr.sort((a, b) => (a.passed - a.year) - (b.passed - b.year));
+}
+
+console.log(yearsLived(inventors))
+
+
+
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+const parisBoulevards = [
+  "Boulevard Auguste-Blanqui",
+  "Boulevard Barbès",
+  "Boulevard Beaumarchais",
+  "Boulevard de l'Amiral-Bruix",
+  "Boulevard Mortier",
+  "Boulevard Poniatowski",
+  "Boulevard Soult",
+  "Boulevard des Capucines",
+  "Boulevard de la Chapelle",
+  "Boulevard de Clichy",
+  "Boulevard du Crime",
+  "Boulevard du Général-d'Armée-Jean-Simon",
+  "Boulevard Haussmann",
+  "Boulevard de l'Hôpital",
+  "Boulevard des Italiens",
+  "Boulevard Lefebvre",
+  "Boulevard de la Madeleine",
+  "Boulevard de Magenta",
+  "Boulevard Malesherbes",
+  "Boulevard Marguerite-de-Rochechouart",
+  "Boulevard Montmartre",
+  "Boulevard du Montparnasse",
+  "Boulevard Raspail",
+  "Boulevard Richard-Lenoir",
+  "Boulevard Saint-Germain",
+  "Boulevard Saint-Michel",
+  "Boulevard de Sébastopol",
+  "Boulevard de Strasbourg",
+  "Boulevard du Temple",
+  "Boulevard Voltaire",
+  "Boulevard Hippolyte-Marquès"
+]
+
+function deBoulevards(arr){
+  return arr.filter(b => b.includes('de'));
+}
+
+console.log(deBoulevards(parisBoulevards));
+
+//they used document.querySelector to create an array from the link list, then filtered the list rather than copying and pasting the array into the document
 
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
 
+function alphabetizeLastNames(arr){
+  return arr.sort();
+}
+
+console.log(alphabetizeLastNames(people));
+
+//they had a much more complicated answer, but this one worked, too
+
+
+
 // 8. Reduce Exercise
 // Sum up the instances of each of these
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+
+function sumOfInstances(arr){
+  let instances = [];
+  const carNumber = arr.filter(item => item === 'car').length;
+  const truckNumber = arr.filter(item => item === 'truck').length;
+  const bikeNumber = arr.filter(item => item === 'bike').length;
+  const walkNumber = arr.filter(item => item === 'walk').length;
+  const vanNumber = arr.filter(item => item === 'van').length;
+
+  instances.push(`car: ${carNumber}`, `truck: ${truckNumber}`, `bike: ${bikeNumber}`, `walk: ${walkNumber}`, `van: ${vanNumber}`);
+
+  return instances;
+}
+
+console.log(sumOfInstances(data));
+
+
+//also had a different answer for this one, but mine still worked
