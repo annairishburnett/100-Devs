2b91d150c8047d3932a55906517b39d55db0ea17
Anna Burnett <annairishburnett@gmail.com>
2025-01-30 16:05:35 -0500

    added more object notes from lecture

--- class25/object-oriented-javascript/js/main.js
+++ class25/object-oriented-javascript/js/main.js
@@ -1,19 +1,105 @@
+// //Factory Function
+// function createCircle(radius){
+//     return {
+//         radius,
+//         draw: function(){
+//             console.log('draw');
+//         }
+//     };
+// }
+// const circle = createCircle(1);
+// // circle.draw()
 
-//Factory Function
-function createCircle(radius){
-    return {
-        radius,
-        draw: function(){
-            console.log('draw');
-        }
-    };
-}
-const circle = createCircle(1);
-// circle.draw()
 
 
+// //Constructor Function
+
+// function Circle(radius){
+//     this.radius = radius;
+//     this.draw = function(){
+//         console.log(draw);
+//     }
+// }
+
+// const another = new Circle(1);
+
+
+
+
+
+// Primitive vs objects and how they behave differently
+
+// let x = 10;
+// let y = x;
+
+// x = 20;
+
+// console.log(x); //20
+// console.log(y); //10
+
+
+// let x = {value: 10};
+// let y = x;
+
+// x.value = 20;
+
+// console.log(x); //20
+// console.log(y); //20
+
+
+
+
+
+//Another example:
+
+// let number = 10;
+
+// function increase(number){
+//     number.value++;
+// }
+
+// increase(number);
+// console.log(number);//10
+
+// //vs.
+
+// let obj = { value: 10};
+
+// function increase(obj){
+//     obj.value++;
+// }
+
+// increase(obj);
+// console.log(obj);//10
+
+
+
+
+
+//Adding/Removing Properties
 
 //Constructor Function
+
+// function Circle(radius){
+//     this.radius = radius;
+//     this.draw = function(){
+//         console.log(draw);
+//     }
+// }
+
+// const circle = new Circle(10);
+
+// circle.location = { x: 1};
+
+// //alternate way to do the same thing as above
+// circle['location'] = { x: 1};
+
+
+
+
+
+
+//Enumerating(iterating) Properties
 
 function Circle(radius){
     this.radius = radius;
@@ -22,11 +108,8 @@ function Circle(radius){
     }
 }
 
-const another = new Circle(1);
+const circle = new Circle(10);
 
-
-
-
-
-
-
+for(let key in circle){
+    console.log(key, circle[key]);
+}
