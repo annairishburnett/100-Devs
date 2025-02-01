f92b65afedaaab929ee409de9422d17d3408d778
Anna Burnett <annairishburnett@gmail.com>
2025-02-01 17:50:27 -0500

    added more notes from the tutorial

--- class25/object-oriented-javascript/js/main.js
+++ class25/object-oriented-javascript/js/main.js
@@ -49,7 +49,6 @@
 
 
 
-
 //Another example:
 
 // let number = 10;
@@ -71,6 +70,11 @@
 
 // increase(obj);
 // console.log(obj);//10
+
+
+
+
+
 
 
 
@@ -99,17 +103,126 @@
 
 
 
+
+
+
+
 //Enumerating(iterating) Properties
+
+// function Circle(radius){
+//     this.radius = radius;
+//     this.draw = function(){
+//         console.log(draw);
+//     }
+// }
+
+// const circle = new Circle(10);
+
+// for(let key in circle){
+//     if(typeof circle[key] !== 'function'){
+//         console.log(key, circle[key]);
+//     }
+// }
+
+// const keys = Object.keys(circle);
+
+// console.log(keys);
+
+
+// if('radius' in circle){
+//     console.log('Circle has a radius');
+// }
+
+
+
+
+
+
+
+
+
+
+//Abstraction
+
+// function Circle(radius){
+//     this.radius = radius;
+
+//     this.defaultLocation = { x: 0, y: 0};
+
+//     this.computeOptimumLocation = function(factor){
+//         //...
+//     }
+
+//     this.draw = function(){
+//         this.computeOptimumLocation(0.1);
+//         console.log(draw);
+//     }
+// }
+
+// const circle = new Circle(10);
+// circle.computeOptimumLocation();
+// circle.draw();
+
+
+
+
+
+
+
+
+
+
+//Private Properties and Methods
+
+// function Circle(radius){
+//     this.radius = radius;
+
+//     let defaultLocation = { x: 0, y: 0};
+
+//     let computeOptimumLocation = function(factor){
+//         //...
+//     }
+
+//     this.draw = function(){
+//       computeOptimumLocation(0.1);
+//       //defaultLocation
+//       // this.radius 
+
+//       console.log('draw');
+//     };
+// }
+
+// const circle = new Circle(10);
+// circle.
+// circle.draw();
+
+
+
+
+
+
+
+
+
+
+
+//Getters and Setters
 
 function Circle(radius){
     this.radius = radius;
-    this.draw = function(){
-        console.log(draw);
+
+    let defaultLocation = { x: 0, y: 0};
+
+    let computeOptimumLocation = function(factor){
+        //...
     }
+
+    this.draw = function(){
+      computeOptimumLocation(0.1);
+
+      console.log('draw');
+    };
 }
 
 const circle = new Circle(10);
-
-for(let key in circle){
-    console.log(key, circle[key]);
-}
+circle.draw();
