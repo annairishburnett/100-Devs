43ee4b441cdc67bd873f6be066d6612702b5eca9
Anna Burnett <annairishburnett@gmail.com>
2025-02-11 15:16:04 -0500

    updated answer

--- class25/object-oriented-javascript/js/main.js
+++ class25/object-oriented-javascript/js/main.js
@@ -208,21 +208,87 @@
 
 //Getters and Setters
 
-function Circle(radius){
-    this.radius = radius;
+// function Circle(radius){
+//     this.radius = radius;
 
-    let defaultLocation = { x: 0, y: 0};
+//     let defaultLocation = { x: 0, y: 0};
 
-    let computeOptimumLocation = function(factor){
-        //...
-    }
+//     this.getDefaultLocation = function(){
+//       return defaultLocation;
+//     };
 
-    this.draw = function(){
-      computeOptimumLocation(0.1);
+//     this.draw = function(){
+//         console.log('draw');
+//     };
 
-      console.log('draw');
-    };
+//     Object.defineProperty(this, 'defaultLocation', {
+//       get: function(){
+//         return defaultLocation;
+//       },
+//       set: function(value){
+//         if (!value.x || !value.y) {
+//           throw new Error('Invalid location.');
+//         }
+//         defaultLocation = value;
+//       }
+//     });
+// }
+
+// const circle = new Circle(10);
+// circle.getDefaultLocation
+// circle.draw();
+
+
+
+
+
+//Task:
+//Create a stopwatch
+//start, stop, reset, duration
+//you can't enter start or stop twice in a row, if you do you get an error, you have to enter start then stop
+
+
+function Stopwatch(){
+  let startTime, endTime, running, duration = 0;
+  
+  this.start = function(){
+    if(running)
+     throw new Error('Stopwatch has already started.');
+  
+    running = true;
+
+    startTime = new Date();
+  };
+  
+  this.stop = function(){
+    if (!running)
+      throw new Error('Stopwatch is not started.');
+
+      running = false;
+
+      endTime = new Date()
+
+      const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+      duration += seconds;
+  };
+  
+  this.reset = function(){
+      startTime = null;
+      endTime = null;
+      running = false;
+      duration = 0;
+  };
+  
+  Object.defineProperty(this, 'duration', {
+    get: function() { return duration; }
+  });
 }
 
-const circle = new Circle(10);
-circle.draw();
+
+const sw = new Stopwatch();
+
+
+
+
+
+
